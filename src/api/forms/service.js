f93bf1546582cb47b644790be86ad86521e93e16
Alex Luckett <alexgluckett@gmail.com>
2024-03-12 13:40:25 +0000

    fix typo

--- src/api/forms/service.js
+++ src/api/forms/service.js
@@ -2,7 +2,7 @@ import { listForms as listFormMetadataEntries } from './form-metadata-repository
 import * as Types from '../api-types.js'
 
 /**
- * Lists the available fforms
+ * Lists the available forms
  * @returns {Promise<Types.FormConfiguration>} form configuration
  */
 const listForms = async () => await listFormMetadataEntries()
