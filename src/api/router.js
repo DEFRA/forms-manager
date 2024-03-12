0770a316faf5f4489af30d2064858da8f7249f5a
Alex Luckett <alexgluckett@gmail.com>
2024-03-12 13:22:59 +0000

    remove example code

--- src/api/router.js
+++ src/api/router.js
@@ -1,12 +1,11 @@
 import { health } from '~/src/api/health'
-import { example } from '~/src/api/example'
 import { forms } from '~/src/api/forms'
 
 const router = {
   plugin: {
     name: 'Router',
     register: async (server) => {
-      await server.register([health, example, forms])
+      await server.register([health, forms])
     }
   }
 }
