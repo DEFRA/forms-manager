15063f9e2535b0134d40d4afd90b75744c31affa
Colin Rotherham <work@colinr.com>
2024-03-14 08:45:53 +0000

    Configure ESLint JSDoc checks
    * Auto-fix alignment
    * Auto-fix hyphens to separate out documentation
    * Require JSDoc only for classes and methods
    * Ensure descriptions and return types are optional

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,11 @@ module.exports = {
   ignorePatterns: ['.server', 'src/__fixtures__', 'coverage'],
   overrides: [
     {
-      extends: ['standard', 'prettier', 'plugin:jsdoc/recommended'],
+      extends: [
+        'standard',
+        'prettier',
+        'plugin:jsdoc/recommended-typescript-flavor'
+      ],
       files: ['**/*.{cjs,js,mjs}'],
       parserOptions: {
         ecmaVersion: 'latest'
@@ -13,7 +17,53 @@ module.exports = {
       plugins: ['prettier', 'jsdoc'],
       rules: {
         'prettier/prettier': 'error',
-        'no-console': 'error'
+        'no-console': 'error',
+
+        // Check for valid formatting
+        'jsdoc/check-line-alignment': [
+          'warn',
+          'never',
+          {
+            tags: ['param', 'property', 'typedef', 'returns']
+          }
+        ],
+
+        // Require hyphens before param description
+        // Aligns with TSDoc style: https://tsdoc.org/pages/tags/param/
+        'jsdoc/require-hyphen-before-param-description': [
+          'warn',
+          'always',
+          {
+            tags: {
+              param: 'always',
+              property: 'always',
+              returns: 'always'
+            }
+          }
+        ],
+
+        // JSDoc blocks are mandatory for classes and methods
+        'jsdoc/require-jsdoc': [
+          'error',
+          {
+            enableFixer: false,
+            require: {
+              ClassDeclaration: true,
+              ClassExpression: true,
+              FunctionExpression: false,
+              MethodDefinition: true
+            }
+          }
+        ],
+
+        // JSDoc @param description is optional
+        'jsdoc/require-param-description': 'off',
+        'jsdoc/require-param': 'error',
+
+        // JSDoc @returns description is optional
+        'jsdoc/require-returns-description': 'off',
+        'jsdoc/require-returns-type': 'off',
+        'jsdoc/require-returns': 'off'
       }
     },
     {
