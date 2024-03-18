cb34e77f5febd1d2db0fb19a74c65e9d5f08e751
Alex Luckett <alexgluckett@gmail.com>
2024-03-18 18:00:40 +0000

    only run import translation in test env

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,32 +3,38 @@ const { NODE_ENV } = process.env
 /**
  * @type {import('@babel/core').TransformOptions}
  */
-module.exports = {
-  browserslistEnv: 'node',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: NODE_ENV === 'test' ? 'auto' : false
-      }
-    ]
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '~': '.'
+module.exports = (/** @type {{ env: (arg0: string) => any; }} */ api) => {
+  const isTest = api.env('test')
+
+  return {
+    browserslistEnv: 'node',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: NODE_ENV === 'test' ? 'auto' : false
         }
-      }
+      ]
     ],
-    ['babel-plugin-transform-import-meta', { module: 'ES6' }]
-  ],
-  env: {
-    development: {
-      sourceMaps: 'inline',
-      retainLines: true
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '~': '.'
+          }
+        }
+      ],
+      ...(isTest
+        ? [['babel-plugin-transform-import-meta', { module: 'ES6' }]]
+        : [])
+    ],
+    env: {
+      development: {
+        sourceMaps: 'inline',
+        retainLines: true
+      }
     }
   }
 }
