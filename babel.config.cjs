683c3f9c35bc870112c60ff61d6d123aba348008
Alex Luckett <alexgluckett@gmail.com>
2024-03-22 17:22:56 +0000

    move babel-plugin-transform-import-meta to env override instead of checking in plugins.

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,38 +3,34 @@ const { NODE_ENV } = process.env
 /**
  * @type {import('@babel/core').TransformOptions}
  */
-module.exports = (/** @type {{ env: (arg0: string) => any; }} */ api) => {
-  const isTest = api.env('test')
-
-  return {
-    browserslistEnv: 'node',
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          modules: NODE_ENV === 'test' ? 'auto' : false
-        }
-      ]
-    ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '~': '.'
-          }
-        }
-      ],
-      ...(isTest
-        ? [['babel-plugin-transform-import-meta', { module: 'ES6' }]]
-        : [])
-    ],
-    env: {
-      development: {
-        sourceMaps: 'inline',
-        retainLines: true
+module.exports = {
+  browserslistEnv: 'node',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: NODE_ENV === 'test' ? 'auto' : false
       }
+    ]
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '~': '.'
+        }
+      }
+    ]
+  ],
+  env: {
+    development: {
+      sourceMaps: 'inline',
+      retainLines: true
+    },
+    test: {
+      plugins: ['babel-plugin-transform-import-meta']
     }
   }
 }
