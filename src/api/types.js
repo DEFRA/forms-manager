8baf746dec769295bbe5a388d04cd4dedbedafbf
Alex Luckett <alexgluckett@gmail.com>
2024-03-25 14:31:17 +0000

    omit id from formconfiguration for formconfigurationinput

--- src/api/types.js
+++ src/api/types.js
@@ -1,5 +1,6 @@
 /**
- * @typedef {object} FormConfigurationInput
+ * @typedef {object} FormConfiguration
+ * @property {string} id - The id of the form
  * @property {string} title - The human-readable title of the form
  * @property {string} organisation - The organisation this form belongs to
  * @property {string} teamName - The name of the team who own this form
@@ -7,10 +8,5 @@
  */
 
 /**
- * @typedef {object} IdMixin
- * @property {string} id - The id of the form
- */
-
-/**
- * @typedef {FormConfigurationInput & { id: string }} FormConfiguration
+ * @typedef {Omit<FormConfiguration, 'id'>} FormConfigurationInput
  */
