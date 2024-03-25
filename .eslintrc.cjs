6103342be5227780a747d747a2d52e021d634cbf
Colin Rotherham <work@colinr.com>
2024-03-25 17:48:53 +0000

    Prefer ESLint warnings for missing types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,6 +38,13 @@ module.exports = {
         'prettier/prettier': 'error',
         'no-console': 'error',
 
+        // Only show warnings for missing types
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+
         // Check import or require statements are A-Z ordered
         'import/order': [
           'error',
