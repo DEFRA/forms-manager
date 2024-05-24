6361355255883d64a79025bb84513c4d26bbae44
Alex Luckett <alexluckett@users.noreply.github.com>
2024-05-24 17:12:13 +0100

    Remove debug code (#140)

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-base-to-string */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import Jwt from '@hapi/jwt'
 
 import { config } from '../config/index.js'
@@ -39,15 +37,6 @@ export const auth = {
         validate: (artifacts) => {
           const user = artifacts.decoded.payload
           const groups = Array.isArray(user?.groups) ? user.groups : []
-
-          // TODO remove
-          logger.debug(
-            `Token header: ${JSON.stringify(artifacts.decoded.header, null, 2)}`
-          )
-          // @ts-expect-error TODO remove
-          logger.debug(`Token payload aud: ${artifacts.decoded.payload.aud}`)
-          // @ts-expect-error TODO remove
-          logger.debug(`Token payload iss: ${artifacts.decoded.payload.iss}`)
 
           logger.debug(`Validating user against groups: ${groups.join(', ')}`)
 
