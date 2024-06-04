78afcb5eee279234c9c0b6000c03a49549660556
Alex Luckett <alexgluckett@gmail.com>
2024-06-04 13:54:38 +0100

    include user in request completion logs

--- src/plugins/log-requests.js
+++ src/plugins/log-requests.js
@@ -9,7 +9,15 @@ export const logRequests = {
   plugin: hapiPino,
   options: {
     ...loggerOptions,
-    log4xxResponseErrors: true
+    log4xxResponseErrors: true,
+    logRequestComplete: true,
+    customRequestCompleteMessage(request, responseTime) {
+      const { user } = request.auth.credentials
+
+      const userPrefix = user ? `[${user.unique_name}]` : ''
+
+      return `[response] ${userPrefix} ${request.method} ${request.path} ${request.raw.res.statusCode} (${responseTime}ms)`
+    }
   }
 }
 
