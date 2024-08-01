a2e6dab370cdefe68c39faa6a2204ec1861ebd51
Kiran Varma <varma.ap@gmail.com>
2024-08-01 10:26:27 +0100

    add error name check

--- src/api/forms/service.js
+++ src/api/forms/service.js
@@ -290,7 +290,7 @@ export async function updateFormMetadata(formId, formUpdate, author) {
 
     return { slug: updatedForm.slug ?? form.slug }
   } catch (err) {
-    if (err.code === 11000) {
+    if (err.name === 'MongoServerError' && err.code === 11000) {
       return {
         error: new Error(`Form title ${formUpdate.title} already exists`)
       }
