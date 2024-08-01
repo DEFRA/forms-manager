0c7578295098d127548cd29524345c261c5f595c
Kiran Varma <varma.ap@gmail.com>
2024-08-01 15:39:23 +0100

    fix lint error

--- src/api/forms/service.js
+++ src/api/forms/service.js
@@ -1,5 +1,6 @@
 import { formDefinitionSchema, slugify } from '@defra/forms-model'
 import Boom from '@hapi/boom'
+import { MongoServerError } from 'mongodb'
 
 import { makeFormLiveErrorMessages } from './constants.js'
 
@@ -290,7 +291,11 @@ export async function updateFormMetadata(formId, formUpdate, author) {
 
     return updatedForm.slug ?? form.slug
   } catch (err) {
-    if (Boom.isBoom(err) && err.code === 11000) {
+    if (
+      Boom.isBoom(err) &&
+      err instanceof MongoServerError &&
+      err.code === 11000
+    ) {
       logger.error(err, `Form title ${formUpdate.title} already exists`)
       throw Boom.badRequest(`Form title ${formUpdate.title} already exists`)
     }
