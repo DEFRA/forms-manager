01f0f0e214295cc65fa8f119329f9ee03ed8d778
David Stone <davidjamesstone@gmail.com>
2024-09-11 14:15:40 +0100

    Add submission guidance to form metadata

--- src/api/forms/constants.js
+++ src/api/forms/constants.js
@@ -5,8 +5,10 @@ export const makeFormLiveErrorMessages = {
     'The questions in this form are not all connected. Connect all pages within the form.',
   missingOutputEmail:
     'The draft form does not contain an output email address. Add an output email address for forms to be sent to.',
-  missingPrivacyNotice:
-    'You need to add a link to the privacy notice before you can publish this form.',
   missingContact:
-    'You must provide at least one way for users to get help before you make this form live.'
+    'You must provide at least one way for users to get help before you make this form live.',
+  missingSubmissionGuidance:
+    'You need to tell your users what happens after they submit their form before you can make it live.',
+  missingPrivacyNotice:
+    'You need to add a link to the privacy notice before you can publish this form.'
 }
