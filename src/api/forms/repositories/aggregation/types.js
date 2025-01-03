6675a425a92f59cd604e361dc991590ae6f79fce
Mohammed Khalid <emailmokhld@gmail.com>
2025-01-03 14:16:41 +0000

    refactor: types

--- src/api/forms/repositories/aggregation/types.js
+++ src/api/forms/repositories/aggregation/types.js
@@ -24,32 +24,37 @@
 
 /**
  * @typedef {object} SwitchExpression
- * @property {Array<SwitchBranch>} branches - Array of condition-value pairs
+ * @property {SwitchBranch[]} branches - Array of condition-value pairs
  * @property {number} default - Default value if no conditions match
  */
 
 /**
- * @typedef {{
- *   $switch: SwitchExpression
- * }} AddFieldsSwitch
+ * @typedef {object} AddFieldsSwitch
+ * @property {SwitchExpression} $switch - MongoDB $switch expression
  */
 
 /**
- * @typedef {{[key: string]: number | {
- *   $dateToString?: {
- *     format: string,
- *     date: string,
- *     timezone: string
- *   },
- *   $switch?: SwitchExpression
- * }}} AddFieldsStage
+ * @typedef {object} DateToStringExpression
+ * @property {string} format - Date format string
+ * @property {string} date - Date field or expression
+ * @property {string} timezone - Timezone for date conversion
+ */
+
+/**
+ * @typedef {object} AddFieldValue
+ * @property {DateToStringExpression} [$dateToString] - MongoDB $dateToString expression
+ * @property {SwitchExpression} [$switch] - MongoDB $switch expression
+ */
+
+/**
+ * @typedef {{[key: string]: number | AddFieldValue}} AddFieldsStage
  */
 
 /**
  * @typedef {object} PipelineStage
  * @property {FilterConditions} [$match] - MongoDB $match stage for filtering documents
  * @property {AddFieldsStage} [$addFields] - MongoDB $addFields stage for adding computed fields to documents
- * @property {{[key: string]: 1 | -1}} [$sort] - MongoDB $sort stage for sorting documents
+ * @property {{ [key: string]: 1 | -1 }} [$sort] - MongoDB $sort stage for sorting documents
  * @property {number} [$skip] - MongoDB $skip stage for pagination
  * @property {number} [$limit] - MongoDB $limit stage for limiting results
  */
