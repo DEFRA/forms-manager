a222b5ca10efa8a8c283604ccd53bf8155f4db11
Chris Cole <whitewaterdesign>
2025-02-18 19:29:35 +0000

    test(manager): 466796 - Add Mongo collection stub

---
+++ src/api/forms/__stubs__/mongo.js
@@ -0,0 +1,76 @@
+/* eslint-env jest */
+export const buildMockCollection = () => {
+  return {
+    bulkWrite: jest.fn(),
+    count: jest.fn(),
+    countDocuments: jest.fn(),
+    deleteMany: jest.fn(),
+    deleteOne: jest.fn(),
+    hint: jest.fn(),
+    insertMany: jest.fn(),
+    insertOne: jest.fn(),
+    replaceOne: jest.fn(),
+    updateMany: jest.fn(),
+    updateOne: jest.fn(),
+    aggregate: jest.fn(),
+    createIndex: jest.fn(),
+    createIndexes: jest.fn(),
+    createSearchIndex: jest.fn(),
+    createSearchIndexes: jest.fn(),
+    distinct: jest.fn(),
+    drop: jest.fn(),
+    dropIndex: jest.fn(),
+    dropIndexes: jest.fn(),
+    dropSearchIndex: jest.fn(),
+    estimatedDocumentCount: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    findOneAndDelete: jest.fn(),
+    findOneAndReplace: jest.fn(),
+    findOneAndUpdate: jest.fn(),
+    indexExists: jest.fn(),
+    indexInformation: jest.fn(),
+    indexes: jest.fn(),
+    initializeOrderedBulkOp: jest.fn(),
+    initializeUnorderedBulkOp: jest.fn(),
+    isCapped: jest.fn(),
+    listIndexes: jest.fn(),
+    listSearchIndexes: jest.fn(),
+    rename: jest.fn(),
+    updateSearchIndex: jest.fn(),
+    options: jest.fn(),
+    watch: jest.fn(),
+    get readConcern() {
+      return undefined
+    },
+    get readPreference() {
+      return undefined
+    },
+    get namespace() {
+      return ''
+    },
+    get writeConcern() {
+      return undefined
+    },
+    get bsonOptions() {
+      return {
+        checkKeys: false,
+        enableUtf8Validation: false,
+        ignoreUndefined: false,
+        raw: false,
+        serializeFunctions: false
+      }
+    },
+    get collectionName() {
+      return ''
+    },
+    get dbName() {
+      return ''
+    }
+  }
+}
+
+/**
+ * @import { FormDefinition } from '@defra/forms-model'
+ * @import { Collection, BSONSerializeOptions } from 'mongodb'
+ */
