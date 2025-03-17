2d32872d2794f2045db56881ee31ce3cff36fa61
Chris Cole <whitewaterdesign>
2025-03-17 12:59:24 +0000

    feat: 481476 - DELETE /forms/{id}/definition/draft/lists/{listId} endpoint

--- src/api/types.js
+++ src/api/types.js
@@ -14,6 +14,7 @@
  * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput, Payload: string[] }>} SortDraftFormPagesRequest
  * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput, Payload: List }>} CreateListDraftFormPagesRequest
  * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput & {listId: string}, Payload: List }>} UpdateListDraftFormPagesRequest
+ * @typedef {Request<{ Server: { db: Db }, Params: FormByIdInput & {listId: string} }>} DeleteListDraftFormPagesRequest
  */
 
 /**
