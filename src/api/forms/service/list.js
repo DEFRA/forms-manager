57243c7a8f1865b720fd11ac7c0bd45d9e1c92f9
Chris Cole <whitewaterdesign>
2025-03-17 09:16:54 +0000

    refactor: 481476 - Create new lists service

---
+++ src/api/forms/service/list.js
@@ -0,0 +1,5 @@
+/**
+ * @import { FormDefinition, FormMetadataAuthor, FormMetadata, FilterOptions, QueryOptions, List } from '@defra/forms-model'
+ * @import { ClientSession } from 'mongodb'
+ * @import { State } from '~/src/api/forms/service/shared.js'
+ */
---
+++ src/api/forms/service/lists.test.js
@@ -0,0 +1,79 @@
+import Boom from '@hapi/boom'
+import { pino } from 'pino'
+
+import { buildList } from '~/src/api/forms/__stubs__/definition.js'
+import * as formDefinition from '~/src/api/forms/repositories/form-definition-repository.js'
+import * as formMetadata from '~/src/api/forms/repositories/form-metadata-repository.js'
+import { formMetadataDocument } from '~/src/api/forms/service/__stubs__/service.js'
+import { addListsToDraftFormDefinition } from '~/src/api/forms/service/definition.js'
+import { getAuthor } from '~/src/helpers/get-author.js'
+import { prepareDb } from '~/src/mongo.js'
+
+jest.mock('~/src/helpers/get-author.js')
+jest.mock('~/src/api/forms/repositories/form-definition-repository.js')
+jest.mock('~/src/api/forms/repositories/form-metadata-repository.js')
+jest.mock('~/src/api/forms/templates.js')
+jest.mock('~/src/mongo.js')
+
+jest.useFakeTimers().setSystemTime(new Date('2020-01-01'))
+describe('lists', () => {
+  const id = '661e4ca5039739ef2902b214'
+  const author = getAuthor()
+  const dateUsedInFakeTime = new Date('2020-01-01')
+  const defaultAudit = {
+    'draft.updatedAt': dateUsedInFakeTime,
+    'draft.updatedBy': author,
+    updatedAt: dateUsedInFakeTime,
+    updatedBy: author
+  }
+
+  const dbMetadataSpy = jest.spyOn(formMetadata, 'update')
+
+  const expectMetadataUpdate = () => {
+    expect(dbMetadataSpy).toHaveBeenCalled()
+    const [formId, updateFilter] = dbMetadataSpy.mock.calls[0]
+    expect(formId).toBe(id)
+    expect(updateFilter.$set).toEqual(defaultAudit)
+  }
+
+  beforeAll(async () => {
+    await prepareDb(pino())
+  })
+
+  beforeEach(() => {
+    jest.mocked(formMetadata.get).mockResolvedValue(formMetadataDocument)
+  })
+
+  describe('addListsToDraftFormDefinition', () => {
+    it('should add a list of lists to the form definition', async () => {
+      const expectedLists = [buildList()]
+      const addListsMock = jest
+        .mocked(formDefinition.addLists)
+        .mockResolvedValueOnce(expectedLists)
+
+      const result = await addListsToDraftFormDefinition(
+        id,
+        expectedLists,
+        author
+      )
+      const [expectedFormId, listToInsert, , state] = addListsMock.mock.calls[0]
+      expect(expectedFormId).toBe(id)
+      expect(listToInsert).toEqual(expectedLists)
+      expect(state).toBeUndefined()
+      expect(result).toEqual(expectedLists)
+      expectMetadataUpdate()
+    })
+    it('should surface errors', async () => {
+      const boomInternal = Boom.internal('Something went wrong')
+      jest.mocked(formDefinition.addLists).mockRejectedValueOnce(boomInternal)
+      await expect(
+        addListsToDraftFormDefinition(id, [buildList()], author)
+      ).rejects.toThrow(boomInternal)
+    })
+  })
+})
+
+/**
+ * @import { FormDefinition, FormMetadata, FormMetadataAuthor, FormMetadataDocument, FormMetadataInput, FilterOptions, QueryOptions, PatchPageFields } from '@defra/forms-model'
+ * @import { WithId } from 'mongodb'
+ */
