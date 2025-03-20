87f099eaaa8debbcd2740d585b6b0d6318f38802
Chris Cole <whitewaterdesign>
2025-03-20 08:57:31 +0000

    refactor: 481476 - Change interface of addPageAtPosition

--- src/api/forms/service/page.js
+++ src/api/forms/service/page.js
@@ -64,19 +64,19 @@ export async function createPageOnDraftDefinition(formId, newPage, author) {
   )
 
   /**
-   * @type {{ position?: number; state?: FormStatus }}
+   * @type {number|undefined}
    */
-  const options = {}
+  let position
 
   if (summaryExists) {
-    options.position = -1
+    position = -1
   }
 
   const page = createPageWithId(newPage)
 
   try {
     await session.withTransaction(async () => {
-      await formDefinition.addPageAtPosition(formId, page, session, options)
+      await formDefinition.addPageAtPosition(formId, page, session, position)
 
       // Set to V2 if not already
       await formDefinition.setEngineVersion(
