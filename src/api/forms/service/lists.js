f8ba7dafb68e82481024df55766a72b24c698b28
Chris Cole <whitewaterdesign>
2025-03-20 08:37:01 +0000

    Revert "refactor: 481476 - Use callback for service - suggestion"
    This reverts commit 0fafc77e352ad4cca45af52286f4481ce5d0d535.

--- src/api/forms/service/lists.js
+++ src/api/forms/service/lists.js
@@ -1,10 +1,6 @@
 import * as formDefinition from '~/src/api/forms/repositories/form-definition-repository.js'
 import * as formMetadata from '~/src/api/forms/repositories/form-metadata-repository.js'
-import {
-  callSessionTransaction,
-  logger,
-  partialAuditFields
-} from '~/src/api/forms/service/shared.js'
+import { logger, partialAuditFields } from '~/src/api/forms/service/shared.js'
 import { client } from '~/src/mongo.js'
 
 /**
@@ -71,21 +67,49 @@ export async function updateListOnDraftFormDefinition(
   list,
   author
 ) {
-  /**
-   * @param {ClientSession} session
-   * @returns {Promise<List>}
-   */
-  const callUpdateListsHandler = (session) =>
-    formDefinition.updateList(formId, listId, list, session)
-
-  return callSessionTransaction(
-    formId,
-    callUpdateListsHandler,
-    author,
-    `Updating list ${listId} on Form Definition (draft) for form ID ${formId}`,
-    `Updated list ${listId} on Form Definition (draft) for form ID ${formId}`,
-    `Failed to update list ${listId} on Form Definition (draft) for form ID ${formId}`
+  logger.info(
+    `Updating list ${listId} on Form Definition (draft) for form ID ${formId}`
   )
+
+  const session = client.startSession()
+
+  try {
+    const updatedList = await session.withTransaction(async () => {
+      // Update the list on the form definition
+      const returnedLists = await formDefinition.updateList(
+        formId,
+        listId,
+        list,
+        session
+      )
+
+      const now = new Date()
+      await formMetadata.update(
+        formId,
+        {
+          $set: partialAuditFields(now, author)
+        },
+        session
+      )
+
+      return returnedLists
+    })
+
+    logger.info(
+      `Updated list ${listId} on Form Definition (draft) for form ID ${formId}`
+    )
+
+    return updatedList
+  } catch (err) {
+    logger.error(
+      err,
+      `Failed to update list ${listId} on Form Definition (draft) for form ID ${formId}`
+    )
+
+    throw err
+  } finally {
+    await session.endSession()
+  }
 }
 
 /**
@@ -133,5 +157,4 @@ export async function removeListOnDraftFormDefinition(formId, listId, author) {
 
 /**
  * @import { FormMetadataAuthor, List } from '@defra/forms-model'
- * @import { ClientSession } from 'mongodb'
  */
