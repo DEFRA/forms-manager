8a9e48c25e62b242c657d80ae189f7ffa15e4e0e
David Stone <davidjamesstone@gmail.com>
2025-06-25 15:36:08 +0100

    Add payload fail action

---
+++ src/helpers/payload-fail-action.js
@@ -0,0 +1,32 @@
+import Boom from '@hapi/boom'
+import Joi from 'joi'
+
+import { InvalidFormDefinitionError } from '~/src/api/forms/errors.js'
+
+/**
+ * Fail action factory to match invalid payloads to InvalidFormDefinitionError
+ * @param {ErrorMatchPath} [errorPathPrefix] - the error path prefix to use for non-root errors
+ */
+export const failAction = (errorPathPrefix) => {
+  /**
+   * @type {Lifecycle.Method}
+   */
+  return (request, h, err) => {
+    if (Boom.isBoom(err) && Joi.isError(err)) {
+      /** @type {{ source?: string } | undefined} */
+      // @ts-expect-error - unknown type
+      const validation = err.output.payload.validation
+      const source = validation?.source ?? ''
+      if (source === 'payload') {
+        throw new InvalidFormDefinitionError(err, errorPathPrefix)
+      }
+    }
+
+    return err ?? new Error('Unknown error')
+  }
+}
+
+/**
+ * @import { Lifecycle } from '@hapi/hapi'
+ * @import { ErrorMatchPath } from '@defra/forms-model'
+ */
