3a0fe7f3300fcc846b5d65e875d6998f7e27a109
whitewater design <whitewaterdesign>
2025-07-25 18:46:41 +0100

    feat: DF-266 - Removing unused code

--- src/messaging/sns.js
+++ src/messaging/sns.js
@@ -1,19 +1,9 @@
-import { ListTopicsCommand, SNSClient } from '@aws-sdk/client-sns'
+import { SNSClient } from '@aws-sdk/client-sns'
 
 import { config } from '~/src/config/index.js'
 
 const awsRegion = config.get('awsRegion')
 const snsEndpoint = config.get('snsEndpoint')
-
-/**
- * Get the topics
- */
-export async function listTopics() {
-  const client = getSNSClient()
-  const command = new ListTopicsCommand({})
-
-  return client.send(command)
-}
 
 /**
  * Retrieves an SNS client
