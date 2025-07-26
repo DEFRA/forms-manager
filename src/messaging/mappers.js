d05386faae158d3ed1402b553089c85cdb5c8587
whitewater design <whitewaterdesign>
2025-07-26 17:17:21 +0100

    feat: DF-271 - Create base mappers for publishing form events

---
+++ src/messaging/mappers.js
@@ -0,0 +1,34 @@
+import { AuditEventMessageSchemaVersion } from '@defra/forms-model'
+
+/**
+ * Helper to create the base message
+ * @param {FormMetadata} metadata
+ * @returns {Omit<MessageBase, 'category'|'type'>}
+ */
+export function createV1MessageBase(metadata) {
+  return {
+    schemaVersion: AuditEventMessageSchemaVersion.V1,
+    entityId: metadata.id,
+    createdAt: metadata.createdAt,
+    createdBy: {
+      id: metadata.createdBy.id,
+      displayName: metadata.createdBy.displayName
+    },
+    messageCreatedAt: new Date()
+  }
+}
+
+/**
+ *
+ * @param {FormMetadata} metadata
+ * @returns {FormMessageDataBase}
+ */
+export function createFormMessageDataBase(metadata) {
+  return {
+    formId: metadata.id,
+    slug: metadata.slug
+  }
+}
+/**
+ * @import { FormMessageDataBase, ChangesMessageData, AuditEventMessageType, FormMessageChangesData, AuditChanges, FormMetadata, MessageBase } from '@defra/forms-model'
+ */
