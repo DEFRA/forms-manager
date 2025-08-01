1ce6af008f2650ea269ec395dba9212123f4eda5
whitewater design <whitewaterdesign>
2025-08-01 16:00:43 +0100

    feat: DF-311 - Create saveToS3 stub

---
+++ src/messaging/s3.js
@@ -0,0 +1,17 @@
+/**
+ * @param {FormDefinition} _formDefinition
+ * @returns {Promise<{
+ *   slug?: string;
+ *   fileId?: string;
+ *   filename?: string;
+ * }>}
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function saveToS3(_formDefinition) {
+  // TODO: implement saveToS3 function
+  return Promise.resolve({})
+}
+
+/**
+ * @import { FormDefinition } from '@defra/forms-model'
+ */
---
+++ src/messaging/s3.test.js
@@ -0,0 +1,13 @@
+import { buildDefinition } from '@defra/forms-model/stubs'
+
+import { saveToS3 } from '~/src/messaging/s3.js'
+
+describe('s3', () => {
+  describe('saveToS3', () => {
+    it('should return an object', async () => {
+      const definition = buildDefinition()
+      const result = await saveToS3(definition)
+      expect(result).toEqual({})
+    })
+  })
+})
