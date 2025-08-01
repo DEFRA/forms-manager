360e06c8c677d75213f06ed6b8f1e9c93c4cc35a
whitewater design <whitewaterdesign>
2025-08-01 17:35:06 +0100

    test: DF-311 - Improve test name

--- src/messaging/s3.test.js
+++ src/messaging/s3.test.js
@@ -4,7 +4,7 @@ import { saveToS3 } from '~/src/messaging/s3.js'
 
 describe('s3', () => {
   describe('saveToS3', () => {
-    it('should return an object', async () => {
+    it('should return undefined when no s3 connection exists', async () => {
       const definition = buildDefinition()
       const result = await saveToS3(definition)
       expect(result).toBeUndefined()
