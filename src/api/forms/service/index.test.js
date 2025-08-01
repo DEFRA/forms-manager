41cbcf5e8b71e3990574b52ae77ca794395a5e82
whitewater design <whitewaterdesign>
2025-08-01 17:12:21 +0100

    feat: DF-311 - Add returns to metadata update

--- src/api/forms/service/index.test.js
+++ src/api/forms/service/index.test.js
@@ -4,6 +4,7 @@ import Boom from '@hapi/boom'
 import { MongoServerError, ObjectId } from 'mongodb'
 import { pino } from 'pino'
 
+import { buildMetadataDocument } from '~/src/api/forms/__stubs__/metadata.js'
 import { InvalidFormDefinitionError } from '~/src/api/forms/errors.js'
 import * as formDefinition from '~/src/api/forms/repositories/form-definition-repository.js'
 import * as formMetadata from '~/src/api/forms/repositories/form-metadata-repository.js'
@@ -247,13 +248,9 @@ describe('Forms service', () => {
 
   describe('updateFormMetadata', () => {
     beforeEach(() => {
-      jest.mocked(formMetadata.update).mockResolvedValue({
-        acknowledged: true,
-        modifiedCount: 1,
-        matchedCount: 1,
-        upsertedCount: 0,
-        upsertedId: null
-      })
+      jest
+        .mocked(formMetadata.update)
+        .mockResolvedValue(buildMetadataDocument())
       jest.mocked(formDefinition.get).mockResolvedValue(definition)
     })
 
@@ -407,13 +404,9 @@ describe('Forms service', () => {
 
   describe('updateFormMetadataV2', () => {
     beforeEach(() => {
-      jest.mocked(formMetadata.update).mockResolvedValue({
-        acknowledged: true,
-        modifiedCount: 1,
-        matchedCount: 1,
-        upsertedCount: 0,
-        upsertedId: null
-      })
+      jest
+        .mocked(formMetadata.update)
+        .mockResolvedValue(buildMetadataDocument())
       jest.mocked(formDefinition.get).mockResolvedValue(definitionV2)
     })
 
