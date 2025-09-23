4967fa764835158ed92fd6b166d5b60f92b8fccf
David Stone <davidjamesstone@gmail.com>
2025-09-23 10:21:05 +0100

    Sonar fix (top-level await)

--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ const logger = createLogger()
 // Move working directory to build output
 chdir(import.meta.dirname)
 
-import('~/src/server.js')
-  .then((server) => server.listen())
-  .catch((/** @type {unknown} */ err) => {
-    logger.info('Server failed to start :(')
-    logger.error(
-      err,
-      `[serverStartup] Server failed to start - ${getErrorMessage(err)}`
-    )
-    throw err
-  })
+try {
+  const server = await import('~/src/server.js')
+  await server.listen()
+} catch (err) {
+  logger.info('Server failed to start :(')
+  logger.error(
+    err,
+    `[serverStartup] Server failed to start - ${getErrorMessage(err)}`
+  )
+  throw err
+}
