d5750491bad257dbcdf8f4046ac1ae388b6dfebb
Alex Luckett <alexgluckett@gmail.com>
2025-10-31 16:35:49 +0000

    Use ESM for migrate-mongo so we can import CDP certs

--- src/secure-context.js
+++ src/secure-context.js
@@ -1,6 +1,7 @@
+/*
+  WARNING: this file is imported by migrate-mongo which does not use babel. Avoid importing assets from the wider project as they'll use tilde imports.
+*/
 import tls from 'node:tls'
-
-import { getTrustStoreCerts } from '~/src/helpers/secure-context/get-trust-store-certs.js'
 
 /**
  * @type {SecureContext}
@@ -9,17 +10,17 @@ export let secureContext
 
 /**
  * Prepares the TLS secure context
- * @param {Server} server
  * @returns
+ * @param {(arg: string) => void} loggerFn
  */
-export function prepareSecureContext(server) {
+export function prepareSecureContext(loggerFn) {
   const originalCreateSecureContext = tls.createSecureContext
 
   tls.createSecureContext = function (options = {}) {
     const trustStoreCerts = getTrustStoreCerts(process.env)
 
     if (!trustStoreCerts.length) {
-      server.logger.info('Could not find any TRUSTSTORE_ certificates')
+      loggerFn('Could not find any TRUSTSTORE_ certificates')
     }
 
     const originalSecureContext = originalCreateSecureContext(options)
@@ -38,6 +39,20 @@ export function prepareSecureContext(server) {
 }
 
 /**
- * @import { Server } from '@hapi/hapi'
+ * Get base64 certs from all environment variables starting with TRUSTSTORE_
+ * @param {NodeJS.ProcessEnv} envs
+ * @returns {string[]}
+ */
+export function getTrustStoreCerts(envs) {
+  return Object.entries(envs)
+    .map(([key, value]) => key.startsWith('TRUSTSTORE_') && value)
+    .filter(
+      /** @returns {envValue is string} */
+      (envValue) => Boolean(envValue)
+    )
+    .map((envValue) => Buffer.from(envValue, 'base64').toString().trim())
+}
+
+/**
  * @import { SecureContext } from 'node:tls'
  */
