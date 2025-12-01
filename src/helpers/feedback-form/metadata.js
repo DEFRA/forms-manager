ae4681fa3584e95b8169a744a9e1a9cb356a515a
Jez Barnsley <jbarnsley.github@gmail.com>
2025-12-01 13:27:51 +0000

    Changed user in metadata

--- src/helpers/feedback-form/metadata.js
+++ src/helpers/feedback-form/metadata.js
@@ -2,23 +2,22 @@
  * This form metadata is reinstated whenever the forms-manmager service startups up.
  * Be aware that any changes to this file will result in changes across all environments as deployments happen.
  */
+const user = {
+  id: '86758ba9-92e7-4287-9751-7705e449688e',
+  displayName: 'Local Service Account'
+}
+
 export const feedbackMetadata = /** @type {FormMetadata} */ ({
   id: '691db72966b1bdc98fa3e72a',
   title: 'Feedback',
   organisation: 'Defra',
   teamName: 'Team Forms',
-  teamEmail: 'jeremy.barnsley@esynergy.co.uk',
+  teamEmail: 'jeremy.barnsley@defra.gov.uk',
   slug: 'feedback',
   createdAt: new Date('2025-11-19T12:25:13.789+00:00'),
-  createdBy: {
-    id: '86758ba9-92e7-4287-9751-7705e449688e',
-    displayName: 'Local Service Account'
-  },
+  createdBy: user,
   updatedAt: new Date('2025-11-26T10:20:12.337+00:00'),
-  updatedBy: {
-    id: '86758ba9-92e7-4287-9751-7705e449688e',
-    displayName: 'Local Service Account'
-  },
+  updatedBy: user,
   versions: [
     {
       versionNumber: 1,
@@ -34,15 +33,9 @@ export const feedbackMetadata = /** @type {FormMetadata} */ ({
   privacyNoticeUrl: 'https://www.gov.uk/help/privacy-notice',
   live: {
     updatedAt: new Date('2025-11-26T10:20:12.337+00:00'),
-    updatedBy: {
-      id: '86758ba9-92e7-4287-9751-7705e449688e',
-      displayName: 'Local Service Account'
-    },
+    updatedBy: user,
     createdAt: new Date('2025-11-19T12:28:30.644+00:00'),
-    createdBy: {
-      id: '86758ba9-92e7-4287-9751-7705e449688e',
-      displayName: 'Local Service Account'
-    }
+    createdBy: user
   }
 })
 
