1a469b6237c8d58f07f85e59156f963aabf4fa7e
Parvesh M <parveshh@gmail.com>
2026-03-27 12:37:40 +0000

    feat: add environment configuration validation changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-import { cwd } from 'process'
+import { cwd } from 'node:process'
 
 import 'dotenv/config'
 import convict from 'convict'
@@ -8,24 +8,28 @@ const isDev = process.env.NODE_ENV !== 'production'
 const isTest = process.env.NODE_ENV === 'test'
 
 export const config = convict({
+  /**@type {SchemaObj<string>} */
   env: {
     doc: 'The application environment.',
     format: ['production', 'development', 'test'],
-    default: 'development',
+    default: null,
     env: 'NODE_ENV'
   },
+  /**@type {SchemaObj<string>} */
   host: {
     doc: 'The IP address to bind',
     format: String,
-    default: '0.0.0.0',
+    default: null,
     env: 'HOST'
   },
+  /**@type {SchemaObj<number>} */
   port: {
     doc: 'The port to bind.',
     format: 'port',
-    default: 3001,
+    default: null,
     env: 'PORT'
   },
+  /**@type {SchemaObj<string>} */
   serviceName: {
     doc: 'Api Service Name',
     format: String,
@@ -34,49 +38,54 @@ export const config = convict({
   serviceVersion: /** @satisfies {SchemaObj<string | null>} */ ({
     doc: 'The service version, this variable is injected into your docker container in CDP environments',
     format: String,
-    nullable: true,
     default: null,
     env: 'SERVICE_VERSION'
   }),
+  /**@type {SchemaObj<string>} */
   root: {
     doc: 'Project root',
     format: String,
     default: cwd()
   },
+  /**@type {SchemaObj<Boolean>} */
   isProduction: {
     doc: 'If this application running in the production environment',
     format: Boolean,
     default: isProduction
   },
+  /**@type {SchemaObj<Boolean>} */
   isDevelopment: {
     doc: 'If this application running in the development environment',
     format: Boolean,
     default: isDev
   },
+  /**@type {SchemaObj<Boolean>} */
   isTest: {
     doc: 'If this application running in the test environment',
     format: Boolean,
     default: isTest
   },
   log: {
+    /**@type {SchemaObj<Boolean>} */
     enabled: {
       doc: 'Is logging enabled',
       format: Boolean,
-      default: !isTest,
+      default: null,
       env: 'LOG_ENABLED'
     },
     level: /** @type {SchemaObj<LevelWithSilent>} */ ({
       doc: 'Logging level',
       format: ['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent'],
-      default: 'info',
+      default: null,
       env: 'LOG_LEVEL'
     }),
     format: /** @type {SchemaObj<'ecs' | 'pino-pretty'>} */ ({
       doc: 'Format to output logs in.',
       format: ['ecs', 'pino-pretty'],
-      default: isProduction ? 'ecs' : 'pino-pretty',
+      default: null,
       env: 'LOG_FORMAT'
     }),
+    /**@type {SchemaObj<string[]>} */
     redact: {
       doc: 'Log paths to redact',
       format: Array,
@@ -86,75 +95,83 @@ export const config = convict({
     }
   },
   mongo: {
+    /** @type {SchemaObj<string>} */
     uri: {
       doc: 'URI for mongodb',
       format: String,
-      default: 'mongodb://127.0.0.1:27017/',
+      default: null,
       env: 'MONGO_URI'
     },
+    /** @type {SchemaObj<string>} */
     databaseName: {
       doc: 'Database name for mongodb',
       format: String,
-      default: 'forms-manager',
+      default: null,
       env: 'MONGO_DATABASE'
     }
   },
+  /** @type {SchemaObj<string>} */
   httpProxy: {
     doc: 'HTTP Proxy',
     format: String,
-    nullable: true,
     default: null,
     env: 'HTTP_PROXY'
   },
+  /** @type {SchemaObj<string>} */
   httpsProxy: {
     doc: 'HTTPS Proxy',
     format: String,
-    default: '',
+    default: null,
     env: 'CDP_HTTPS_PROXY'
   },
+  /** @type {SchemaObj<boolean>} */
   isSecureContextEnabled: {
     doc: 'Enable Secure Context',
     format: Boolean,
-    default: isProduction,
+    default: null,
     env: 'ENABLE_SECURE_CONTEXT'
   },
+  /** @type {SchemaObj<boolean>} */
   isMetricsEnabled: {
     doc: 'Enable metrics reporting',
     format: Boolean,
-    default: isProduction,
+    default: null,
     env: 'ENABLE_METRICS'
   },
-  /**
-   * @todo We plan to replace `node-convict` with `joi` and remove all defaults.
+  /*
    * These OIDC/roles are for the DEV application in the DEFRA tenant.
    */
+  /** @type {SchemaObj<string>} */
   oidcJwksUri: {
     doc: 'The URI that defines the OIDC json web key set',
     format: String,
-    default:
-      'https://login.microsoftonline.com/770a2450-0227-4c62-90c7-4e38537f1102/discovery/v2.0/keys',
+    default: null,
     env: 'OIDC_JWKS_URI'
   },
+  /** @type {SchemaObj<string>} */
   oidcVerifyAud: {
     doc: 'The audience used for verifying the OIDC JWT',
     format: String,
-    default: 'ec32e5c5-75fa-460a-a359-e3e5a4a8f10e',
+    default: null,
     env: 'OIDC_VERIFY_AUD'
   },
+  /** @type {SchemaObj<string>} */
   oidcVerifyIss: {
     doc: 'The issuer used for verifying the OIDC JWT',
     format: String,
-    default:
-      'https://login.microsoftonline.com/770a2450-0227-4c62-90c7-4e38537f1102/v2.0',
+    default: null,
     env: 'OIDC_VERIFY_ISS'
   },
+  /** @type {SchemaObj<string>} */
   roleEditorGroupId: {
     doc: 'The AD security group the access token needs to claim membership of',
     format: String,
-    default: '9af646c4-fa14-4606-8ebf-ec187ac03386',
+    default: null,
     env: 'ROLE_EDITOR_GROUP_ID'
   },
   tracing: {
+    /** @type {SchemaObj<string>} */
+
     header: {
       doc: 'CDP tracing header name',
       format: String,
@@ -162,58 +179,67 @@ export const config = convict({
       env: 'TRACING_HEADER'
     }
   },
+  /** @type {SchemaObj<string>} */
   awsRegion: {
     doc: 'AWS region',
     format: String,
-    default: 'eu-west-2',
+    default: null,
     env: 'AWS_REGION'
   },
+  /** @type {SchemaObj<string>} */
   snsEndpoint: {
     doc: 'The SNS endpoint, if required (e.g. a local development dev service)',
     format: String,
-    default: '',
+    default: null,
     env: 'SNS_ENDPOINT'
   },
+  /** @type {SchemaObj<string>} */
   snsTopicArn: {
     doc: 'SNS topic ARN',
     format: String,
-    default: '',
+    default: null,
     env: 'SNS_TOPIC_ARN'
   },
+  /** @type {SchemaObj<boolean>} */
   publishAuditEvents: {
     doc: 'Publish audit events for forms-audit-api',
     format: Boolean,
-    default: !isProduction,
+    default: null,
     env: 'FEATURE_FLAG_PUBLISH_AUDIT_EVENTS'
   },
+  /** @type {SchemaObj<string>} */
   s3Endpoint: {
     doc: 'The S3 HTTP(S) endpoint, if required (e.g. a local development dev service). Activating this will force path style addressing for compatibility with Localstack.',
     format: String,
-    default: '',
+    default: null,
     env: 'S3_ENDPOINT'
   },
+  /** @type {SchemaObj<string>} */
   s3Bucket: {
     doc: 'S3 bucket name',
     format: String,
-    default: '',
+    default: null,
     env: 'FORM_DEF_BUCKET_NAME'
   },
+  /** @type {SchemaObj<string>} */
   entitlementUrl: {
     doc: 'Forms entitlements API URL',
     format: String,
-    default: 'http://localhost:3004',
+    default: null,
     env: 'ENTITLEMENT_URL'
   },
+  /** @type {SchemaObj<boolean>} */
   useEntitlementApi: {
     doc: 'Feature flag to enable entitlement API for fetching scopes',
     format: Boolean,
-    default: false,
+    default: null,
     env: 'FEATURE_FLAG_USE_ENTITLEMENT_API'
   },
+  /** @type {SchemaObj<string>} */
   publicKeyForSecrets: {
     doc: 'Public key for encryption of secrets',
     format: String,
-    default: '',
+    default: null,
     env: 'PUBLIC_KEY_FOR_SECRETS'
   }
 })
